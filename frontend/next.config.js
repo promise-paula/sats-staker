014501ba22175bfe99cedb1670c8a4fcdbf29875
promise-paula <paula.promise2@gmail.com>
2025-12-27 15:06:58 +0100

    Refactor code structure for improved readability and maintainability

---
+++ frontend/next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: { 
+    unoptimized: true,
+    domains: ['images.pexels.com']
+  },
+  experimental: {
+    esmExternals: false,
+  },
+};
+
+module.exports = nextConfig;
